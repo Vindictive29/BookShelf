e98434ed3e1340d138341c6190d5cc37dd4784bc
David Low <djlow12977@hotmail.com>
2016-07-21 09:30:42 -0400

    linked static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,32 +3,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | BookShelf"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | BookShelf"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | BookShelf"
   end
 
-  test "should get contacts" do
-    get static_pages_contacts_url
+  test "should get contact" do
+    get contact_path
     assert_response :success
-    assert_select "title", "Contacts | BookShelf"
+    assert_select "title", "Contact | BookShelf"
   end
 end
