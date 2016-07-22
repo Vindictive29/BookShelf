92ebbaa983e90f6398bc750a9c45174effc2bfc3
David Low <djlow12977@hotmail.com>
2016-07-22 06:10:28 -0400

    database and controller for 'users' created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720144519) do
+ActiveRecord::Schema.define(version: 20160722100830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
